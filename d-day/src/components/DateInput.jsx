6f71c6af2a377d41ae8e3dfb2af0f5aa14b3465d
Hyoin-kim <hi980506@naver.com>
2021-10-25 17:52:32 +0900

    feat : D-day 구현 단계

---
+++ d-day/src/components/DateInput.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const DateInput = ({ year, month, date, setYear, setMonth, setDate }) => {
+
+    const todayDate = () => {
+        const now = new Date();
+        setYear(now.getFullYear());
+        setMonth(now.getMonth() + 1);
+        setDate(now.getDate());
+    }
+
+    const handleChange = (e, setState) => {
+        setState(e.target.value);
+      };
+
+    return (
+        <div className="dateInput">
+            <button className="todey__button" onClick={todayDate}>오늘</button>
+            <div className="date-input__form">
+                <input type="text" className="date-input date-input__year" onChange={(e)=>handleChange(e,setYear)}></input>년
+                <input type="text" className="date-input" onChange={(e)=>handleChange(e,setMonth)}></input>월
+                <input type="text" className="date-input" onChange={(e)=>handleChange(e,setDate)}></input>일을 기준으로
+            </div>
+        </div>
+    );
+};
+
+export default DateInput;
---
+++ d-day/src/components/common/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            Hyoin's D-day 계산기
+        </div>
+    );
+};
+
+export default Header;
